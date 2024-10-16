1cb9a8bfee0205efd4f1848ab815151709569790
Kindi Lerning <KINDI.learn@gmail.com>
2024-10-16 12:20:20 +0530

    Loader Size updated

--- app/profile/loading.jsx
+++ app/profile/loading.jsx
@@ -2,11 +2,11 @@ export default function Loading() {
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-[#eaeaf5]">
-        <video autoPlay loop muted className="w-screen min-h-screen">
+        <video autoPlay loop muted className="flex justify-center items-center">
           <source
             src="preloader.mp4"
             type="video/mp4"
-            className="w-screen min-h-screen"
+            // className="w-screen min-h-screen"
           />
         </video>
       </div>
