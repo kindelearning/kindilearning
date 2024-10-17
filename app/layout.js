2f69841abd1bbaa8e183b4c899ac43692d6f6b1b
Kindi Lerning <KINDI.learn@gmail.com>
2024-10-17 19:29:25 +0530

    PWA Initiated

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "./context/CartContext";
-
+import Head from "next/head";
 
 /**
  * @ToDo  TO be removed as we dont use this, but need to check it infuture
@@ -23,13 +23,27 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <head>
+        <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-48x48.png"
+            sizes="48x48"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           {/* Google CDN for all the fonts */}
           <link
             href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;700&family=Gloria+Hallelujah&family=Montserrat:wght@400;700&display=swap"
             rel="stylesheet"
           />
-        </head>
+        </Head>
 
         <body
           className={cn(
