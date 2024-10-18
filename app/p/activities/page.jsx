b32f45e3f9bdd0d5547310ed34143b8de2ae779e
Kindi Lerning <KINDI.learn@gmail.com>
2024-10-18 11:50:35 +0530

    Updated the Filters Preview

--- app/p/activities/page.jsx
+++ app/p/activities/page.jsx
@@ -479,88 +479,13 @@ export default async function ActivitiesPage() {
               {/* Activity grid begin */}
               <div className="flex w-full flex-col gap-2">
                 <div className="grid grid-cols-2 w-full gap-2 md:gap-4 justify-between items-start">
-                  {/* <div className="flex flex-col w-full gap-2 md:gap-4 justify-between items-start">
-                  Display filtered activities based on Features
-                  {selectedFeatures && filteredActivities.length > 0 && (
-                    <div className="w-full gap-2 grid grid-cols-2">
-                      {filteredActivities.map((activity) => (
-                        <div key={activity.id}>
-                          <article className="rounded-lg ">
-                            <Link
-                              target="_blank"
-                              href={`/p/activities/${activity.id}`}
-                            >
-                              <div className="md:w-full hover:shadow-md duration-200 min-w-[170px] w-[170px] min-h-[250px] h-full bg-white items-start justify-start border rounded-3xl flex flex-col md:flex-row gap-4">
-                                <div className="claracontainer w-full flex-col justify-start items-center gap-7 inline-flex">
-                                  <div className="w-full max-w-[180px] lg:max-w-full h-auto">
-                                    <div className="flex max-h-[180px] min-h-[150px] h-[150px] lg:min-h-[276px] lg:h-full lg:max-h-[276px] md:max-h-[300px] overflow-clip rounded-t-3xl">
-                                      <Image
-                                        width={280}
-                                        height={250}
-                                        alt={activity.title}
-                                        className="w-full max-h-[180px] duration-300 hover:scale-105 lg:min-h-[276px] lg:h-full lg:max-h-[276px] md:max-h-[300px] object-cover rounded-t-3xl"
-                                        src={activity.thumbnail.url}
-                                      />
-                                    </div>
-                                    <div className="w-full p-2 flex-col justify-start lg:p-4 items-start flex gap-2 md:gap-2 lg:gap-4">
-                                      <div className="flex-col w-full gap-[6px] justify-start items-start">
-                                        <div className="text-[#0a1932] text-[16px] md:text-xl font-semibold font-fredoka leading-[20px]">
-                                          {activity.title}
-                                        </div>
-                                        <div className="justify-start w-full items-center gap-1 lg:gap-2 inline-flex">
-                                          <div className="text-[#0a1932] min-w-[max-content] p-0 lg:pl-2 md:text-[18px] md:leading-[22px] font-[500] font-fredoka text-[10px] lg:text-[16px] leading-none">
-                                            {activity.setUpTime}
-                                          </div>
-                                          <ul className="text-[#0a1932] justify-between items-center gap-4 flex px-2 lg:text-[16px] text-[10px] font-normal font-fredoka list-disc leading-none">
-                                            {activity.skills
-                                              .slice(0, 2)
-                                              .map((skill, index) => (
-                                                <li key={index}>
-                                                  {skill.slice(0, 8)}
-                                                </li>
-                                              ))}
-                                          </ul>
-                                        </div>
-                                      </div>
-                                      <div className="items-center justify-center gap-2 md:gap-4 grid grid-cols-5">
-                                        <Image
-                                          className="w-[20px] h-[24px] lg:w-[48px] lg:h-[48px]"
-                                          src={SpeechLanguageActivity}
-                                        />
-                                        <Image
-                                          className="w-[20px] h-[24px] lg:w-[48px] lg:h-[48px]"
-                                          src={DiscoveringOurWorldActivity}
-                                        />
-                                        <Image
-                                          className="w-[20px] h-[24px] lg:w-[48px] lg:h-[48px]"
-                                          src={ReadingWritingActivity}
-                                        />
-                                        <Image
-                                          className="w-[20px] h-[24px] lg:w-[48px] lg:h-[48px]"
-                                          src={ExperimentsMathActivity}
-                                        />
-                                        <div
-                                          className={`w-[20px] lg:w-[48px] lg:h-[48px] h-[20px] flex lg:rounded-[12px] justify-center items-center bg-[#F6BEBF] rounded-[4px]`}
-                                        >
-                                          <span className="text-red p-[2px] text-[12px] lg:text-[20px] font-medium font-fredoka">
-                                            +1
-                                          </span>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </Link>
-                          </article>
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div> */}
-
                   {/* Render filtered activities first */}
-                  {selectedFeatures && filteredActivities.length > 0 ? (
+                  {selectedFeatures &&
+                  selectedAgeFocus &&
+                  selectedPrepTime &&
+                  selectedTheme &&
+                  selectedSkilCategory &&
+                  filteredActivities.length > 0 ? (
                     filteredActivities.map((activity) => (
                       <div className="w-full flex flex-col gap-4 ">
                         <div key={activity.id}>
