dc2de4e6469030e5e860d50dc091dc81106dbdc6
Kindi Lerning <KINDI.learn@gmail.com>
2024-10-23 13:01:24 +0530

    levels Completed

--- app/Sections/Profile/ProfileSection.jsx
+++ app/Sections/Profile/ProfileSection.jsx
@@ -482,6 +482,168 @@ const PartnerList = ({ userId }) => {
   );
 };
 
+/**
+ *
+ * @param {MyActivity completed by User} param0
+ * @returns
+ */
+const MyLevel = ({ userID }) => {
+  const [activities, setActivities] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchActivities = async () => {
+    const query = `
+      query GetUserActivities($relationalFirst: Int, $where: AccountWhereUniqueInput!) {
+        values: account(where: $where) {
+          id
+          username
+          myActivity(first: $relationalFirst) {
+            id
+            title
+            documentInStages(includeCurrent: true) {
+              id
+              stage
+              updatedAt
+              publishedAt
+            }
+          }
+        }
+      }
+    `;
+
+    const variables = {
+      relationalFirst: 10,
+      where: { id: userID },
+    };
+
+    try {
+      const response = await fetch(HYGRAPH_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${HYGRAPH_TOKEN}`,
+        },
+        body: JSON.stringify({ query, variables }),
+      });
+
+      const result = await response.json();
+
+      if (result.errors) {
+        throw new Error(result.errors[0].message);
+      } else {
+        setActivities(result.data.values.myActivity);
+      }
+    } catch (error) {
+      setError("Error fetching activities: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, [userID]);
+
+  const getUserLevel = (activityCount) => {
+    if (activityCount >= 0 && activityCount <= 5) {
+      return 1; // Level 1
+    } else if (activityCount > 5 && activityCount <= 10) {
+      return 2; // Level 2
+    } else if (activityCount > 10 && activityCount <= 15) {
+      return 3; // Level 3
+    } else if (activityCount > 15 && activityCount <= 20) {
+      return 4; // Level 4
+    } else if (activityCount > 20 && activityCount <= 25) {
+      return 5; // Level 5
+    } else {
+      return "Max Level"; // More than 25
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  const userLevel = getUserLevel(activities.length);
+  const progressPercentage = (activities.length / 25) * 100;
+
+  return (
+    <div className="flex w-full flex-col justify-start items-center gap-2">
+      <div className="w-full claracontainer flex flex-row gap-2 justify-start items-center">
+        <div className="text-[#3f3a64] clarabodyTwo">
+          User Level: {userLevel}
+        </div>
+        <Dialog className="bg-[#EAEAF5] w-full rounded-[28px] claracontainer">
+          <DialogTrigger asChild>
+            <Badge
+              className="text-[10px] md:text-[16px] cursor-pointer"
+              variant="outline"
+            >
+              Check Now
+            </Badge>
+          </DialogTrigger>
+          <DialogContent className="bg-[#EAEAF5] max-h-[70%] overflow-scroll p-0 overflow-x-hidden rounded-[28px] w-full claracontainer">
+            <DialogHeader className="p-4">
+              <div className="flex flex-row justify-center items-center w-full">
+                <DialogTitle>
+                  <div className="text-center">
+                    <span className="text-[#3f3a64] text-[24px] md:text-[36px] font-semibold font-fredoka capitalize  ">
+                      My{" "}
+                    </span>
+                    <span className="text-red text-[24px] md:text-[36px] font-semibold font-fredoka capitalize  ">
+                      Level
+                    </span>
+                  </div>
+                </DialogTitle>
+              </div>
+            </DialogHeader>
+            <DialogDescription className="flex w-full px-4 claracontainer flex-col justify-start items-center">
+              <div className="flex flex-col justify-center items-center w-full claracontainer gap-4">
+                <LevelCard level="Level 1" activities="5" />
+                <LevelCard level="Level 2" activities="10" />
+                <LevelCard level="Level 3" activities="15" />
+                <LevelCard level="Level 4" activities="20" />
+                <LevelCard level="Level 5" activities="25" />
+              </div>
+            </DialogDescription>
+            <DialogFooter className="sticky  rounded-t-[16px] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] bottom-0 m-0 w-full px-4 bg-[#ffffff]">
+              <PopupFooter PrimaryText="Save and Continue" />
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="flex w-full gap-1 items-center">
+        <div
+          className="progress-bar-container"
+          style={{
+            width: "100%",
+            backgroundColor: "#e0e0e0",
+            borderRadius: "5px",
+            // marginTop: "10px",
+            // color:'#f15c57'
+          }}
+        >
+          <div
+            className="progress-bar"
+            style={{
+              width: `${progressPercentage}%`,
+              backgroundColor: "#f15c57",
+              height: "10px",
+              borderRadius: "5px",
+              // color:'#f15c57'
+            }}
+          ></div>
+        </div>
+        <p className="clarabodyTwo w-[max-content] min-w-[120px]">
+          Activities: {activities.length}
+        </p>
+      </div>
+      {/* <p style={{ marginTop: "5px", color: "#555" }}>
+        {Math.round(progressPercentage)}% completed
+      </p> */}
+    </div>
+  );
+};
 
 export default async function ProfileSection() {
   const { data: session, status } = useSession();
@@ -540,7 +702,6 @@ export default async function ProfileSection() {
         </div>
         <div className="claracontainer bg-[#F5F5F5] md:bg-[#EAEAF5] -mt-4 rounded-t-[12px] z-2 lg:m-12 px-4 py-6 rounded-xl md:px-2 lg:p-8 xl:p-12 w-full flex flex-col overflow-hidden gap-[20px]">
           {/* Top Profile Card */}
-          {/* {profileData ? <MyProgressActivity userId={profileData.id} /> : <></>} */}
           <div className="w-full flex bg-[white] rounded-[24px] p-2 md:p-4 justify-start items-start gap-[4px] lg:gap-[12px] lg:items-center">
             <div className="w-fit lg:max-w-[160px] lg:w-full items-center flex justify-start">
               {profileData ? (
@@ -611,46 +772,9 @@ export default async function ProfileSection() {
               </div>
               <div className="flex flex-col w-full gap-1 items-start justify-start">
                 <div className="flex flex-row w-full justify-start items-center gap-2">
-                  <div className="text-[#3f3a64] clarabodyTwo">Level 1</div>
+                  {/* <div className="text-[#3f3a64] clarabodyTwo">User Level: {userLevel}</div> */}
                   {/* Trigger for the Level Popup */}
-                  <Dialog className="bg-[#EAEAF5] w-full rounded-[28px] claracontainer">
-                    <DialogTrigger asChild>
-                      <Badge
-                        className="text-[10px] md:text-[16px] cursor-pointer"
-                        variant="outline"
-                      >
-                        Check Now
-                      </Badge>
-                    </DialogTrigger>
-                    <DialogContent className="bg-[#EAEAF5] max-h-[70%] overflow-scroll p-0 overflow-x-hidden rounded-[28px] w-full claracontainer">
-                      <DialogHeader className="p-4">
-                        <div className="flex flex-row justify-center items-center w-full">
-                          <DialogTitle>
-                            <div className="text-center">
-                              <span className="text-[#3f3a64] text-[24px] md:text-[36px] font-semibold font-fredoka capitalize  ">
-                                My{" "}
-                              </span>
-                              <span className="text-red text-[24px] md:text-[36px] font-semibold font-fredoka capitalize  ">
-                                Level
-                              </span>
-                            </div>
-                          </DialogTitle>
-                        </div>
-                      </DialogHeader>
-                      <DialogDescription className="flex w-full px-4 claracontainer flex-col justify-start items-center">
-                        <div className="flex flex-col justify-center items-center w-full claracontainer gap-4">
-                          <LevelCard level="Level 1" activities="5" />
-                          <LevelCard level="Level 2" activities="10" />
-                          <LevelCard level="Level 3" activities="15" />
-                          <LevelCard level="Level 4" activities="20" />
-                          <LevelCard level="Level 5" activities="25" />
-                        </div>
-                      </DialogDescription>
-                      <DialogFooter className="sticky  rounded-t-[16px] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] bottom-0 m-0 w-full px-4 bg-[#ffffff]">
-                        <PopupFooter PrimaryText="Save and Continue" />
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
+                  {profileData ? <MyLevel userID={profileData.id} /> : null}
                   <Link
                     href="/profile/update"
                     className="flex lg:hidden"
@@ -663,16 +787,6 @@ export default async function ProfileSection() {
                       Edit
                     </Badge>
                   </Link>
-                </div>
-                <div className="w-full flex flex-row justify-between items-center clarabodyTwo gap-2">
-                  <Slider
-                    disabled
-                    defaultValue={[33]}
-                    max={100}
-                    className="h-[8px] text-[#3a3a89]"
-                    step={10}
-                  />{" "}
-                  Activities
                 </div>
               </div>
             </div>
