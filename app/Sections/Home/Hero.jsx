2615c405831381c19cc7650ed900ad3b223dae19
Kindi Learning <KINDI.learn@gmail.com>
2024-11-21 15:41:06 +0530

    udpated

--- app/Sections/Home/Hero.jsx
+++ app/Sections/Home/Hero.jsx
@@ -16,7 +16,7 @@ export default async function Hero() {
   }
   const videoUrl = homeData[0]?.heroFeaturedVideo?.url;
   return (
-    <section className="w-full min-h-screen h-full md:min-h-[600px] md:h-full lg:h-full bg-[#4b4097] py-12 md:py-24 lg:py-28 items-center justify-center flex flex-col md:flex-row gap-[20px]">
+    <section className="w-full min-h-screen h-full md:min-h-[600px] md:h-full lg:h-full bg-[#4b4097] pt-4 pb-12 md:py-24 lg:py-28 items-start md:items-center justify-center flex flex-col md:flex-row gap-[20px]">
       <div className="claracontainer w-full flex flex-col-reverse justify-between md:items-center lg:flex-row px-0 md:px-2 lg:px-0 xl:px-0 bg-[#4b4097] xl:flex-row gap-8 md:gap-0 lg:gap-4 ">
         <div className="h-auto w-full lg:max-w-[60%] md:min-w-[50%] md:w-[100%] lg:w-full flex-col px-4 md:px-0 lg:px-4 justify-center items-start gap-6 md:gap-7 lg:gap-8 xl:gap-10 inline-flex ">
           <div className="w-full flex flex-col justify-start items-start h-auto gap-6 md:gap-2 lg:gap-4 xl:gap-4 ">
@@ -51,7 +51,7 @@ export default async function Hero() {
           </div>
         </div>
         <div className="w-full flex md:min-w-[300px] items-start justify-center h-fit min-h-[400px] md:w-[300px] lg:w-full ">
-          <div className="w-full h-[460px] animate-fade-in md:max-w-[500px] flex items-end justify-end">
+          <div className="w-full h-fit md:h-[460px] animate-fade-in md:max-w-[500px] flex items-end justify-end">
             <div className="hero-video-container">
               <video
                 autoPlay
