5c13ba03835f5de737452ad0e2c3506690280cf6
Kindi Learning <KINDI.learn@gmail.com>
2024-11-21 15:32:27 +0530

    Home page Video updated

--- app/Sections/Home/Hero.jsx
+++ app/Sections/Home/Hero.jsx
@@ -1,55 +1,9 @@
 "use client";
 
 import NotFound from "@/app/not-found";
-import { Button } from "@/components/ui/button";
 import { getHomeData } from "@/lib/hygraph";
-import {
-  HomeHero,
-  HomeHeroFour,
-  HomeHeroOne,
-  HomeHeroThree,
-  HomeHeroTwo,
-} from "@/public/Images";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
-import { useState, useEffect } from "react";
-
-const HeroImageGif = [
-  { id: 1, src: HomeHeroOne },
-  { id: 2, src: HomeHeroTwo },
-  { id: 3, src: HomeHeroThree },
-  { id: 4, src: HomeHeroFour },
-  { id: 4, src: HomeHero },
-];
-
-const ImageSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === HeroImageGif.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000); // Change image every 3 seconds
-
-    return () => clearInterval(intervalId);
-  }, [HeroImageGif.length]);
-
-  return (
-    <div className="w-full md:min-w-[300px] block md:w-[300px] lg:w-full justify-center items-center h-auto animate-fade-in relative">
-      {HeroImageGif.map((image, index) => (
-        <Image
-          key={image.id}
-          alt="Kindi"
-          src={image.src}
-          className={`absolute w-[full h-[400px] items-center top-0 left-0 md:min-w-[300px] md:w-[400px] lg:w-full object-contain ${
-            index === currentIndex ? "opacity-100" : "opacity-0"
-          } transition-opacity duration-500`}
-        />
-      ))}
-    </div>
-  );
-};
+import { Button } from "@/components/ui/button";
 
 export default async function Hero() {
   const { data: session, status } = useSession();
@@ -57,16 +11,19 @@ export default async function Hero() {
   if (!homeData || !homeData[0]?.hero) {
     return <NotFound />;
   }
-
+  if (!homeData || !homeData[0]?.heroFeaturedVideo) {
+    return <NotFound />;
+  }
+  const videoUrl = homeData[0]?.heroFeaturedVideo?.url;
   return (
-    <section className="w-full min-h-screen h-full md:min-h-[600px] md:h-full lg:h-full bg-purple py-12 md:py-24 lg:py-28 items-center justify-center flex flex-col md:flex-row gap-[20px]">
-      <div className="claracontainer w-full flex flex-col-reverse justify-between md:items-center lg:flex-row px-0 md:px-2 lg:px-0 xl:px-0 bg-purple xl:flex-row gap-8 md:gap-0 lg:gap-4 ">
-        <div className="h-auto w-full md:min-w-[50%] md:w-[100%] lg:w-full flex-col px-4 md:px-0 lg:px-4 justify-center items-start gap-6 md:gap-7 lg:gap-8 xl:gap-10 inline-flex ">
+    <section className="w-full min-h-screen h-full md:min-h-[600px] md:h-full lg:h-full bg-[#4b4097] py-12 md:py-24 lg:py-28 items-center justify-center flex flex-col md:flex-row gap-[20px]">
+      <div className="claracontainer w-full flex flex-col-reverse justify-between md:items-center lg:flex-row px-0 md:px-2 lg:px-0 xl:px-0 bg-[#4b4097] xl:flex-row gap-8 md:gap-0 lg:gap-4 ">
+        <div className="h-auto w-full lg:max-w-[60%] md:min-w-[50%] md:w-[100%] lg:w-full flex-col px-4 md:px-0 lg:px-4 justify-center items-start gap-6 md:gap-7 lg:gap-8 xl:gap-10 inline-flex ">
           <div className="w-full flex flex-col justify-start items-start h-auto gap-6 md:gap-2 lg:gap-4 xl:gap-4 ">
             <div className="text-white animate-text-reveal clarascript ">
               Early Learning for a Lifetime of Achievement
             </div>
-            <div className="flex flex-col w-full justify-start items-start gap-3 lg:gap-6 animate-fade-in ">
+            <div className="flex flex-col flex-wrap w-full justify-start items-start gap-3 lg:gap-6 animate-fade-in ">
               <div className="w-full lg:w-[max-content]">
                 <span className="text-white claraheading lg:text-[50px] lg:leading-[56px]  font-bold font-fredoka capitalize animate-fade-in">
                   Brain Development Activities
@@ -94,7 +51,18 @@ export default async function Hero() {
           </div>
         </div>
         <div className="w-full flex md:min-w-[300px] items-start justify-center h-fit min-h-[400px] md:w-[300px] lg:w-full ">
-          <ImageSlider />
+          <div className="w-full h-[460px] animate-fade-in md:max-w-[500px] flex items-end justify-end">
+            <div className="hero-video-container">
+              <video
+                autoPlay
+                loop
+                muted
+                className="flex justify-center items-center"
+              >
+                <source src={videoUrl} type="video/mp4" />
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
