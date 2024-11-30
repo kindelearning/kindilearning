6813441bdc626222ec543417c14cfe762cb60c84
Kindi Learning <KINDI.learn@gmail.com>
2024-11-30 10:13:32 +0530

    updated to resolve white screen error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Optional but recommended
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'media.graphassets.com', // Replace with your domain(s)
+        protocol: "https",
+        hostname: "media.graphassets.com", // Replace with your domain(s)
       },
     ],
     dangerouslyAllowSVG: true,
@@ -14,6 +15,14 @@ const nextConfig = {
         hostname: "**.graphassets.com",
       },
     ],
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.alias["react"] = "react/cjs/react.development.js";
+      config.resolve.alias["react-dom"] =
+        "react-dom/cjs/react-dom.development.js";
+    }
+    return config;
   },
 };
 
