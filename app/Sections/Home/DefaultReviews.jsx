c69ea71861e1347cec24b2b33a1ebf072bda94b8
Kindi Learning <KINDI.learn@gmail.com>
2024-12-17 12:25:34 +0530

    Default Review Connected to Strapi

--- app/Sections/Home/DefaultReviews.jsx
+++ app/Sections/Home/DefaultReviews.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { fetchDefaultReview } from "@/app/data/p/Home";
 import { getDefaultReview } from "@/lib/hygraph";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import React, { useState, useEffect } from "react";
@@ -7,32 +8,44 @@ import React, { useState, useEffect } from "react";
 const DefaultReviews = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0); // Track the current review index
 
+  // Use useEffect to fetch data when the component mounts
   useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await getDefaultReview();
-      // console.log("Default review data:", fetchedData);
-      setData(fetchedData);
-      setLoading(false);
+    const loadData = async () => {
+      const fetchedData = await fetchDefaultReview(); // Fetch data
+      if (fetchedData) {
+        setData(fetchedData); // Set the data if available
+      } else {
+        setError("Failed to fetch data");
+      }
+      setLoading(false); // Stop loading
     };
 
-    fetchData();
-  }, []);
+    loadData();
+  }, []); // Empty dep
 
-  if (loading) return <div className="bg-[#eaeaf5]">Loading...</div>;
+  // Loading state
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  if (!data || data.length === 0)
-    return <div className="bg-[#eaeaf5]">No data available</div>;
+  // Error state
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   // Function to handle next navigation
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.Content.length);
   };
 
   // Function to handle previous navigation
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + data.Content.length) % data.Content.length
+    );
   };
 
   // Function to handle dot navigation
@@ -40,75 +53,88 @@ const DefaultReviews = () => {
     setCurrentIndex(index);
   };
 
-  const currentReview = data[currentIndex];
-
   return (
-    <div
-      className="flex flex-col gap-[20px] justify-center items-center w-full"
-      style={{
-        padding: "20px",
-        backgroundColor: currentReview.bgColor?.hex || "#3f3a64",
-        transition: "background-color 0.5s ease-in-out",
-      }}
-    >
-      <section
-        className="w-full py-6 cursor-pointer items-center justify-between flex flex-row gap-1 lg:gap-[20px] transition-all duration-300 animate-fade-in"
-        style={{
-          backgroundColor: currentReview.bgColor?.hex || "#3f3a64",
-          transition: "background-color 0.5s ease-in-out",
-        }}
-      >
-        <button
-          onClick={handlePrev}
-          // className="p-1 lg:p-2 lg:border-2 border-white rounded-full transition"
-          className=" w-[32px] h-[32px] hidden lg:flex justify-center items-center left-0  transform -translate-y-1/2 bg-white bg-opacity-30 backdrop-blur-lg text-[#000000] p-2 rounded-full z-10"
+    <>
+      {data?.Content && data.Content.length > 0 && (
+        <div
+          className="flex flex-col gap-[20px] justify-center lg:py-6 items-center w-full"
+          style={{
+            backgroundColor: data.Content[currentIndex]?.bgcolor || "#3f3a64",
+            transition: "background-color 0.5s ease-in-out",
+          }}
         >
-          <ChevronLeft />
-        </button>
-        <div className="claracontainer w-full flex-col duration-150 animate-fade-in justify-center items-center inline-flex md:py-6 sm:py-4">
-          <h2
-            className="text-3xl text-center font-fredoka duration-150 animate-fade-in font-bold md:text-2xl sm:text-xl"
+          <div
+            className="w-full py-6 lg:py-2 lg:px-4 cursor-pointer items-center justify-between flex flex-row gap-1 lg:gap-[20px] transition-all duration-300 animate-fade-in"
             style={{
-              animation: "fadeIn 1s ease-in-out",
-              animationFillMode: "forwards",
+              backgroundColor: data.Content[currentIndex]?.bgcolor || "#3f3a64",
+              transition: "background-color 0.5s ease-in-out",
             }}
           >
-            <p className="text-purple text-center">{currentReview.titleOne}</p>
-            <p className="text-white text-center">{currentReview.titleTwo}</p>
-          </h2>
-          <span
-            className="text-lg text-white text-center md:text-base duration-150 animate-fade-in sm:text-sm"
-            style={{
-              animation: "fadeIn 1s ease-in-out 0.5s",
-              animationFillMode: "forwards",
-            }}
-          >
-            {currentReview.content}
-          </span>
-        </div>
-        <button
-          onClick={handleNext}
-          className=" w-[32px] h-[32px] hidden lg:flex justify-center items-center right-0 transform -translate-y-1/2 bg-white bg-opacity-30 backdrop-blur-lg text-[#000000] p-2 rounded-full z-10"
+            <button
+              onClick={handlePrev}
+              className=" w-[32px] h-[32px] hidden lg:flex justify-center items-center left-0  transform -translate-y-1/2 bg-white bg-opacity-30 backdrop-blur-lg text-[#000000] p-2 rounded-full z-10"
+            >
+              <ChevronLeft />
+            </button>
+            <div className="claracontainer w-full flex-col duration-150 animate-fade-in justify-center items-center inline-flex md:py-6 sm:py-4">
+              <h2
+                className="text-3xl text-center font-fredoka duration-150 animate-fade-in font-bold md:text-2xl sm:text-xl"
+                style={{
+                  animation: "fadeIn 1s ease-in-out",
+                  animationFillMode: "forwards",
+                }}
+              >
+                <p className="text-purple text-center">
+                  {data.Content[currentIndex]?.featuredText && (
+                    <p>{data.Content[currentIndex]?.featuredText}</p>
+                  )}
+                </p>
+                <p className="text-white text-center">
+                  {data.Content[currentIndex]?.Title && (
+                    <p>{data.Content[currentIndex]?.Title}</p>
+                  )}
+                </p>
+              </h2>
+              <span
+                className="text-lg text-white text-center md:text-base duration-150 animate-fade-in sm:text-sm"
+                style={{
+                  animation: "fadeIn 1s ease-in-out 0.5s",
+                  animationFillMode: "forwards",
+                }}
+              >
+                {data.Content[currentIndex]?.body && (
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: data.Content[currentIndex]?.body, // Render HTML content from rich text or Markdown
+                    }}
+                  />
+                )}
+              </span>
+            </div>
+            <button
+              onClick={handleNext}
+              className=" w-[32px] h-[32px] hidden lg:flex justify-center items-center right-0 transform -translate-y-1/2 bg-white bg-opacity-30 backdrop-blur-lg text-[#000000] p-2 rounded-full z-10"
+            >
+              <ChevronRight />
+            </button>
+          </div>
 
-          // className="p-1 lg:p-2 lg:border-2 border-white rounded-full transition"
-        >
-          <ChevronRight />
-        </button>
-      </section>
-      {/* Dot Navigation */}
-      <div className="flex mt-4 space-x-2">
-        {data.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => handleDotClick(index)}
-            className={`w-3 h-3 rounded-full ${
-              index === currentIndex ? "bg-blue-600" : "bg-gray-400"
-            } transition`}
-            aria-label={`Go to review ${index + 1}`}
-          />
-        ))}
-      </div>
-    </div>
+          {/* Dot Navigation */}
+          <div className="flex mt-4 space-x-2">
+            {data.Content.map((_, index) => (
+              <button
+                key={index}
+                onClick={() => handleDotClick(index)}
+                className={`w-3 h-3 rounded-full ${
+                  index === currentIndex ? "bg-red" : "bg-[white]"
+                } transition`}
+                aria-label={`Go to review ${index + 1}`}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- app/data/p/Home/index.js
+++ app/data/p/Home/index.js
@@ -81,26 +81,27 @@ export async function fetchEarlyLearningExpert() {
 
 export async function fetchPopularLearning() {
   try {
-    const response = await fetch('http://localhost:1337/api/popularlearning?populate=*');
-    
+    const response = await fetch(
+      "http://localhost:1337/api/popularlearning?populate=*"
+    );
+
     if (!response.ok) {
-      throw new Error(`Error fetching Popular Learning: ${response.statusText}`);
+      throw new Error(
+        `Error fetching Popular Learning: ${response.statusText}`
+      );
     }
-    
+
     const data = await response.json();
     return data?.data || null;
   } catch (error) {
-    console.error('Error fetching Popular Learning:', error);
+    console.error("Error fetching Popular Learning:", error);
     return null;
   }
 }
 
-
 export async function fetchHowItWorksData() {
   try {
-    const res = await fetch(
-      `http://localhost:1337/api/howitwork?populate=*`
-    );
+    const res = await fetch(`http://localhost:1337/api/howitwork?populate=*`);
     if (!res.ok) {
       throw new Error("Failed to fetch How It Works data");
     }
@@ -117,7 +118,9 @@ export async function fetchHowItWorksData() {
 
 export async function fetchMonthlyTheme() {
   try {
-    const res = await fetch('http://localhost:1337/api/monthlytheme?populate=*');
+    const res = await fetch(
+      "http://localhost:1337/api/monthlytheme?populate=*"
+    );
 
     if (!res.ok) {
       throw new Error(`Error: ${res.status} - ${res.statusText}`);
@@ -132,6 +135,27 @@ export async function fetchMonthlyTheme() {
     return data.data; // Return the entire content of Monthly Theme
   } catch (error) {
     console.error("Error fetching Monthly Theme data:", error.message);
+    return null;
+  }
+}
+
+export async function fetchDefaultReview() {
+  try {
+    const response = await fetch(
+      "http://localhost:1337/api/defaultreview?populate=*"
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching Default Review: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    if (!data || !data.data) {
+      throw new Error("No data found for Monthly Theme section");
+    }
+    return data?.data || null;
+  } catch (error) {
+    console.error("Error fetching Default Review:", error);
     return null;
   }
 }
