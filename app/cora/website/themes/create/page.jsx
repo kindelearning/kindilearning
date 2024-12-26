e8cccdaabc559e2740e370c3bcaf067216d246d4
Kindi Learning <KINDI.learn@gmail.com>
2024-12-26 16:52:19 +0530

    Create theme litle optimised but thumbnail is not working

--- app/cora/website/themes/create/page.jsx
+++ app/cora/website/themes/create/page.jsx
@@ -119,179 +119,177 @@ export default function CreateTheme() {
   };
 
   return (
-    <section className="p-8 font-fredoka bg-gray-50 min-h-screen flex justify-center items-center">
-      <div className="container mx-auto  bg-white shadow-lg rounded-lg p-8">
-        {/* Header with navigation buttons */}
-        <div className="flex justify-between mb-6">
-          <Button
-            onClick={() => router.push("/cora/website/themes")} // Navigate to All Themes page
-            className="text-[#282828] bg-transparent hover:bg-transparent hover:scale-105 ease-out duration-150"
-          >
-            Back to All Themes
-          </Button>
-          {themeCreated && documentId && (
-            <Link
-              href={`http://localhost:3000/p/our-themes/${documentId}`}
-              target="_blank"
-              className="text-purple hover:scale-105 duration-150"
+    <>
+      <head>
+        <title>Create New Theme | Cora</title>
+      </head>
+      <section className="p-8 font-fredoka bg-gray-50 min-h-screen flex justify-center items-center">
+        <div className="container mx-auto  bg-white shadow-lg rounded-lg p-8">
+          {/* Header with navigation buttons */}
+          <div className="flex justify-between mb-6">
+            <Button
+              onClick={() => router.push("/cora/website/themes")} // Navigate to All Themes page
+              className="text-[#282828] bg-transparent hover:bg-transparent hover:scale-105 ease-out duration-150"
             >
-              Preview on Live Website
-            </Link>
-          )}
-          <Button
-            type="submit"
-            onClick={handleSubmit}
-            disabled={themeCreated}
-            className="text-green bg-transparent hover:bg-transparent hover:scale-105 ease-out duration-150"
-          >
-            Create a New Theme
-          </Button>
-        </div>
+              Back to All Themes
+            </Button>
+            {themeCreated && documentId && (
+              <Link
+                href={`http://localhost:3000/p/our-themes/${documentId}`}
+                target="_blank"
+                className="text-purple hover:scale-105 duration-150"
+              >
+                Preview on Live Website
+              </Link>
+            )}
+            <Button
+              type="submit"
+              onClick={handleSubmit}
+              disabled={themeCreated}
+              className="text-green bg-transparent hover:bg-transparent hover:scale-105 ease-out duration-150"
+            >
+              Create a New Theme
+            </Button>
+          </div>
 
-        <h2 className="text-3xl font-bold text-gray-800 mb-6">
-          Create New Theme
-        </h2>
+          <h2 className="text-3xl font-bold text-gray-800 mb-6">
+            Create New Theme
+          </h2>
 
-        {/* Form */}
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-row justify-between gap-2">
-            <div className="flex flex-col w-full">
-              <div className="mb-6">
-                <label
-                  htmlFor="title"
-                  className="block text-lg font-medium text-gray-700 mb-2"
-                >
-                  Title:
-                </label>
-                <Input
-                  id="title"
-                  placeholder="Title"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                  className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
+          {/* Form */}
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-row justify-between gap-2">
+              <div className="flex flex-col w-full">
+                <div className="mb-6">
+                  <label
+                    htmlFor="title"
+                    className="block text-lg font-medium text-gray-700 mb-2"
+                  >
+                    Title:
+                  </label>
+                  <Input
+                    id="title"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="metaDesc"
+                    className="block text-lg font-medium text-gray-700 mb-2"
+                  >
+                    Meta Description:
+                  </label>
+                  <Input
+                    id="metaDesc"
+                    placeholder="Meta Description:"
+                    value={metaDesc}
+                    onChange={(e) => setMetaDesc(e.target.value)}
+                    className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
               </div>
 
-              <div className="mb-6">
+              <div className="mb-6 w-full">
                 <label
-                  htmlFor="metaDesc"
+                  htmlFor="thumbnail"
                   className="block text-lg font-medium text-gray-700 mb-2"
                 >
-                  Meta Description:
+                  Thumbnail:
                 </label>
-                <Input
-                  id="metaDesc"
-                  placeholder="Meta Description:"
-                  value={metaDesc}
-                  onChange={(e) => setMetaDesc(e.target.value)}
-                  className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
+                <div
+                  className="flex flex-col w-full justify-start items-center p-2 border-2 border-dashed border-gray-300 rounded-md cursor-pointer transition-all hover:border-primary"
+                  onDragOver={handleDragOver}
+                  onDrop={handleDrop}
+                >
+                  <input
+                    type="file"
+                    id="thumbnail"
+                    onChange={handleThumbnailChange}
+                    className="border w-full rounded-xljustify-start opacity-30"
+                  />
+                  {!thumbnail ? (
+                    <div className="text-center text-gray-500">
+                      <p>
+                        Drag and drop an image here, or click to select a file
+                      </p>
+                    </div>
+                  ) : (
+                    <div className="w-full object-cover h-[300px] bg-gray-200 rounded-md overflow-hidden">
+                      <img
+                        src={URL.createObjectURL(thumbnail)}
+                        alt="Thumbnail Preview"
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="mb-6 w-full">
+            <div className="mb-6">
               <label
-                htmlFor="thumbnail"
+                htmlFor="mainContent"
                 className="block text-lg font-medium text-gray-700 mb-2"
               >
-                Thumbnail:
+                Main Content:
               </label>
-              <div
-                className="flex flex-col w-full justify-start items-center p-2 border-2 border-dashed border-gray-300 rounded-md cursor-pointer transition-all hover:border-primary"
-                onDragOver={handleDragOver}
-                onDrop={handleDrop}
-              >
-                <input
-                  type="file"
-                  id="thumbnail"
-                  onChange={handleThumbnailChange}
-                  className="border w-full rounded-xljustify-start opacity-30"
-                />
-                {!thumbnail ? (
-                  <div className="text-center text-gray-500">
-                    <p>
-                      Drag and drop an image here, or click to select a file
-                    </p>
-                  </div>
-                ) : (
-                  <div className="w-full object-cover h-[300px] bg-gray-200 rounded-md overflow-hidden">
-                    <img
-                      src={URL.createObjectURL(thumbnail)}
-                      alt="Thumbnail Preview"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                )}
-              </div>
+              
+              <ClaraMarkdownRichEditor
+                onChange={handleMainContentChange2}
+                value={mainContent}
+                className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              />
             </div>
-          </div>
 
-          <div className="mb-6">
-            <label
-              htmlFor="mainContent"
-              className="block text-lg font-medium text-gray-700 mb-2"
+            <div className="mb-6">
+              <label
+                htmlFor="launchTime"
+                className="block text-lg font-medium text-gray-700 mb-2"
+              >
+                Launch Time:
+              </label>
+              <Input
+                type="datetime-local"
+                id="launchTime"
+                value={launchTime}
+                onChange={(e) => setLaunchTime(e.target.value)}
+                className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+              className="w-full p-3 text-white bg-blue-600 hover:bg-blue-700 rounded-md transition"
             >
-              Main Content:
-            </label>
-            <Textarea
-              placeholder="Main Content:"
-              id="mainContent"
-              value={mainContent}
-              onChange={handleMainContentChange}
-              className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              rows={6}
-            />
-            <ClaraMarkdownRichEditor
-              onChange={handleMainContentChange2}
-              value={mainContent}
-              className="mt-2 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
+              {loading ? "Creating..." : "Create Theme"}
+            </Button>
+          </form>
 
-          <div className="mb-6">
-            <label
-              htmlFor="launchTime"
-              className="block text-lg font-medium text-gray-700 mb-2"
-            >
-              Launch Time:
-            </label>
-            <Input
-              type="datetime-local"
-              id="launchTime"
-              value={launchTime}
-              onChange={(e) => setLaunchTime(e.target.value)}
-              className="p-3 border border-gray-300 rounded-md w-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          {/* Submit Button */}
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={loading}
-            className="w-full p-3 text-white bg-blue-600 hover:bg-blue-700 rounded-md transition"
-          >
-            {loading ? "Creating..." : "Create Theme"}
-          </Button>
-        </form>
-
-        {/* Custom Dialog */}
-        <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-          <DialogContent>
-            <DialogTitle>{dialogMessage.title}</DialogTitle>
-            <DialogDescription>{dialogMessage.description}</DialogDescription>
-            {/* <Button onClick={() => setOpenDialog(false)}>Close</Button> */}
-            <DialogFooter className="sm:justify-start">
-              <DialogClose asChild>
-                <Button type="button" variant="secondary">
-                  Close
-                </Button>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
-    </section>
+          {/* Custom Dialog */}
+          <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+            <DialogContent>
+              <DialogTitle>{dialogMessage.title}</DialogTitle>
+              <DialogDescription>{dialogMessage.description}</DialogDescription>
+              {/* <Button onClick={() => setOpenDialog(false)}>Close</Button> */}
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary">
+                    Close
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </section>
+    </>
   );
 }
